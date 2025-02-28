2085c328ee0dc307ceec0a486390812dd880c5e0
DevShoaib58 <devshoaib58@gmail.com>
2025-02-28 18:04:10 +0500

    Basic Structure ,Conneting DB,Setup UI and header footer

---
+++ includes/footer.php
@@ -0,0 +1,48 @@
+<section class="ezy__copyright4 light pt-5 pb-5">
+  <div class="container">
+    <div class="text-center">
+      <p class="mb-0">Copyright all rights reserved</p>
+    </div>
+  </div>
+</section>
+
+<style>
+.ezy__copyright4 {
+  /* Bootstrap variables */
+  --bs-body-color: #28303b;
+  --bs-body-bg: rgb(255, 255, 255);
+
+  /* easy frontend variables */
+  --ezy-theme-color: rgb(13, 110, 253);
+  --ezy-theme-color-rgb: 13, 110, 253;
+
+  background-color: var(--bs-body-bg);
+  color: var(--bs-body-color);
+}
+
+/* Gray Block Style */
+.gray .ezy__copyright4,
+.ezy__copyright4.gray {
+  /* Bootstrap variables */
+  --bs-body-bg: rgb(246, 246, 246);
+}
+
+/* Dark Gray Block Style */
+.dark-gray .ezy__copyright4,
+.ezy__copyright4.dark-gray {
+  /* Bootstrap variables */
+  --bs-body-color: #ffffff;
+  --bs-body-bg: rgb(30, 39, 53);
+}
+
+/* Dark Block Style */
+.dark .ezy__copyright4,
+.ezy__copyright4.dark {
+  /* Bootstrap variables */
+  --bs-body-color: #ffffff;
+  --bs-body-bg: rgb(11, 23, 39);
+}
+</style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php include 'includes/header.php'; ?>
+<?php
+require_once 'includes/config.php';
+
+include 'includes/login.php';
+?>
+<?php include 'includes/footer.php'; ?>
